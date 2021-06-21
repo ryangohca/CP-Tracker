c5201d409d2060edf1aa4a0d871892484ba2548e
Ryan Goh <56359753+ryangohca@users.noreply.github.com>
2021-06-21 11:49:06 +0000

    more accurate resizing of cards

--- static/js/homePage.js
+++ static/js/homePage.js
@@ -5,6 +5,26 @@ function openTab(contentClass, contentId) {
     document.getElementById(contentId).style.display = "block";
 }
 
+function getLengthCardContainer(){
+    for (var content of document.getElementsByClassName("cardcontainer")) {
+        if (content.style.display !== "none") return content.offsetWidth;
+    }
+}
+
+function normaliseCardLength(cardLength){
+    var containerLen = getLengthCardContainer();
+    console.log(containerLen);
+    var expectedCards = Math.floor(containerLen / cardLength);
+    for (var content of document.getElementsByClassName("collectionCard")) {
+        content.style.width = (containerLen / expectedCards) - 30 + "px"; // 30px offset because of margin
+    }
+}
+
 window.onload = function(){
     openTab("collectionscontainer", "publicCollectionsDiv");
+    normaliseCardLength(350);
 };
+
+window.onresize = function(){
+    normaliseCardLength(350);
+}
