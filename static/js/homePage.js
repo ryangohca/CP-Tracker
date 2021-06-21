c94589bf7840abfe27e97f3eb7a06a1c9cf3ce4f
Ryan Goh <56359753+ryangohca@users.noreply.github.com>
2021-06-21 16:25:31 +0000

    fixed bug where cards height gets truncated to 0px after switching tabs

--- static/js/homePage.js
+++ static/js/homePage.js
@@ -16,19 +16,32 @@ function normaliseCardLength(cardLength){
     var expectedCards = Math.round(containerLen / cardLength);
     var maxHeight = 0;
     for (var content of document.getElementsByClassName("collectionCard")) {
+        if (content.parentElement.parentElement.style.display === 'none') continue;
         content.style.width = (containerLen / expectedCards) - 30 + "px"; // 30px offset because of margin
         maxHeight = Math.max(content.offsetHeight, maxHeight);
     }
     for (var content of document.getElementsByClassName("collectionCard")) {
+        if (content.parentElement.parentElement.style.display === 'none') continue;
         content.style.height = maxHeight + "px"; // 30px offset because of margin
     }
 }
 
+function openModal(modalId){
+    document.getElementById(modalId).style.display='block';
+}
+
+function closeModal(modalId){
+    document.getElementById(modalId).style.display='none';
+}
+
+function openCollectionsTab(tabID){
+    openTab("collectionscontainer", tabID);
+    normaliseCardLength(cardLength);
+}
 const cardLength = 360;
 window.onload = function(){
-    openTab("collectionscontainer", "publicCollectionsDiv");
-    normaliseCardLength(cardLength);
-};
+    openCollectionsTab("publicCollectionsDiv");
+}
 
 window.onresize = function(){
     normaliseCardLength(cardLength);
